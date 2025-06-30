97c87d83673779e91fba533856253a4ed7164afb
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2025-06-30 23:34:38 +0200

    Create c123.c

---
+++ c123.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+  char name[100];
+  printf("What's your name?");
+  fgets(name,sizeof(name),stdin);
+  printf("hello,%s",name);
+  return 0;
+}
+
+
+#include <stdio.c>
+
+int main(void)
+{
+
+  char name[100];
+  printf("What's your name? ");
+
+  fgets(name,sizeof(name),stdin);
+  printf("Hello,%s", name);
+  return 0;
+  
+}
